c72d3ec1c43ac5128af66f3c2686acafc5bdc899
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-04-18 02:04:07 -0400

    Update minigame to Minecraft 1.18.2

--- src/main/java/io/github/haykam821/downpour/Main.java
+++ src/main/java/io/github/haykam821/downpour/Main.java
@@ -14,10 +14,10 @@ public class Main implements ModInitializer {
 	public static final GameType<DownpourConfig> DOWNPOUR_TYPE = GameType.register(DOWNPOUR_ID, DownpourConfig.CODEC, DownpourWaitingPhase::open);
 
 	private static final Identifier ROUNDS_SURVIVED_ID = new Identifier(MOD_ID, "rounds_survived");
-	public static final StatisticKey<Integer> ROUNDS_SURVIVED = StatisticKey.intKey(ROUNDS_SURVIVED_ID, StatisticKey.StorageType.TOTAL);
+	public static final StatisticKey<Integer> ROUNDS_SURVIVED = StatisticKey.intKey(ROUNDS_SURVIVED_ID);
 
 	private static final Identifier PLAYERS_PUNCHED_ID = new Identifier(MOD_ID, "players_punched");
-	public static final StatisticKey<Integer> PLAYERS_PUNCHED = StatisticKey.intKey(PLAYERS_PUNCHED_ID, StatisticKey.StorageType.TOTAL);
+	public static final StatisticKey<Integer> PLAYERS_PUNCHED = StatisticKey.intKey(PLAYERS_PUNCHED_ID);
 
 	@Override
 	public void onInitialize() {
